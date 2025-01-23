627ee34784e35f21395c62c6755d47f0257cf8bd
segni_abera <segniab69@gmail.com>
2025-01-23 10:12:19 +0300

    this is a simple calculator

---
+++ calculator.js
@@ -0,0 +1,46 @@
+function add(number1, number2){
+    return number1 + number2;
+}
+
+function subtract(number1, number2){
+    return number1 - number2;
+}
+
+function multiply(number1, number2){
+    return number1 * number2;
+}
+
+function divide(number1, number2){
+    if(number2===0){
+        alert("error!!!");
+    }
+    else
+    return number1 / number2;
+}
+document.getElementById('add').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value);
+const number2 = parseFloat(document.getElementById('number2').value);
+    const result = add(number1, number2);
+    document.getElementById('result').innerText = result;
+ 
+});
+document.getElementById('subtract').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value);
+    const number2 = parseFloat(document.getElementById('number2').value);
+    const result = subtract(number1, number2);
+    document.getElementById('result').innerText = result;
+});
+
+document.getElementById('multiply').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value);
+    const number2 = parseFloat(document.getElementById('number2').value);
+    const result = multiply(number1, number2);
+    document.getElementById('result').innerText = result;
+});
+
+document.getElementById('divide').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value);
+    const number2 = parseFloat(document.getElementById('number2').value);
+    const result = divide(number1, number2);
+    document.getElementById('result').innerText = result;
+});
